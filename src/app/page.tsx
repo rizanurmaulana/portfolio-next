065dbaa3c94391ca29d46c8931ead27a1b98ddd3
rizanurmaulana <rizanurmaulana2810@gmail.com>
2025-09-11 12:04:32 +0700

    feat: update animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -210,6 +210,7 @@ export default function Home() {
               className="w-full p-4 border border-gray-100 rounded-xl"
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true, amount: 0.5 }}
               transition={{ duration: 0.5, delay: index * 0.1 }}
             >
               <img
